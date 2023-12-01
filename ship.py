692ede71558b329381cdbe34cb6e2a88f43c4049
josue.ramirez <jramirez6964@yahoo.com>
2023-11-30 21:38:12 -0800

    Add comments to better understand ship.py.

--- ship.py
+++ ship.py
@@ -3,13 +3,18 @@ import pygame
 class Ship:
     """A class to manage the ship."""
 
+    # The __init__ method has a parameter that is a reference to the current
+    # instance of the AlienInvasion class because we want the Ship class to
+    # access game resources defined in AlienInvasion.
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
-        self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+        self.screen = ai_game.screen  # Assign the screen to an attribute of Ship.
+        # Get the screen's rect attribute and assign it to the Ship.
+        self.screen_rect = ai_game.screen.get_rect() # Makes it easy to place the ship.
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
+        # Access the picture's rect to assign it to the Ship.
         self.rect = self.image.get_rect()
 
         # Start each new ship at the bottom center of the screen.
