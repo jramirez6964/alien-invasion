4391705441e31ea2234d4b7a4d6640a09341e7df
josue.ramirez <jramirez6964@yahoo.com>
2023-12-02 14:54:40 -0800

    Match the background color of the snail image so that it blends in with the background color of the game screen.

--- snail.py
+++ snail.py
@@ -11,7 +11,8 @@ class Snail:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load('images/snail.bmp')
+        self.image = pygame.image.load('images/snail.bmp').convert()
+        self.image.set_colorkey((255, 255, 255))
 
         self.rect = self.image.get_rect()
 
