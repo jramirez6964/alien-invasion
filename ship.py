3fe53c7a8284e30697e41ca169242ed0ba55a5fb
josue.ramirez <jramirez6964@yahoo.com>
2023-12-02 19:10:20 -0800

    Use a new float attribute to finely control the ship's position.

--- ship.py
+++ ship.py
@@ -9,6 +9,7 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen  # Assign the screen to an attribute of Ship.
+        self.settings = ai_game.settings
         # Get the screen's rect attribute and assign it to the Ship.
         self.screen_rect = ai_game.screen.get_rect() # Makes it easy to place the ship.
 
@@ -20,16 +21,23 @@ class Ship:
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a float for the ship's exact horizontal position.
+        self.x = float(self.rect.x)
+
         # Movement flags; start with a ship that's not moving.
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """Update the ship's position based on movement flags."""
+        # Update the ship's x value, not the rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location."""
