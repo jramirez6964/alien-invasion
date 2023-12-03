0b1867322dc8f1e81ba492d1c8a8b0ec08c7fce6
josue.ramirez <jramirez6964@yahoo.com>
2023-12-02 19:29:39 -0800

    Edit the game so it runs in fullscreen mode.

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,8 +16,9 @@ class AlienInvasion:
         self.settings = Settings()
 
         # Create a display window to draw the game's graphical elements.
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         # The attribute called screen is called a surface.
         pygame.display.set_caption("Alien Invasion")
 
