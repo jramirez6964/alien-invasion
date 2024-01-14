f5d2c91b364c0ededf0f3f63b2d32dc5c94c7072
Josue Ramirez <jramirez6964@yahoo.com>
2024-01-13 19:45:16 -0800

    Remove bullets when they leave screen
    Added the logic so that the bullets will be removed from the game when they leave the screen on the right.

--- alien_invasion.py
+++ alien_invasion.py
@@ -88,7 +88,7 @@ class AlienInvasion:
 
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
-            if bullet.rect.bottom <= 0:
+            if bullet.rect.left >= self.settings.screen_width:
                 self.bullets.remove(bullet)
 
 
