11cc9c28e74f22e05d6189964371d8a78233d18e
Josue Ramirez <jramirez6964@yahoo.com>
2024-01-13 18:56:38 -0800

    Update ship.py
    Column character limit formatting.

--- ship.py
+++ ship.py
@@ -8,10 +8,12 @@ class Ship:
     # access game resources defined in AlienInvasion.
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
-        self.screen = ai_game.screen  # Assign the screen to an attribute of Ship.
+        self.screen = ai_game.screen  # Assign the screen to an attribute of 
+        # Ship.
         self.settings = ai_game.settings
         # Get the screen's rect attribute and assign it to the Ship.
-        self.screen_rect = ai_game.screen.get_rect() # Makes it easy to place the ship.
+        self.screen_rect = ai_game.screen.get_rect() # Makes it easy to place 
+        # the ship.
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
